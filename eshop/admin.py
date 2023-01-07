ff44b8c7558d9c5c5a23925b2b0125bb881b690e
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-07 12:02:08 +0000

    Add of model category

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Image
+from .models import *
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Image)
+admin.site.register(Category)
--- eshop/models.py
+++ eshop/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     description = models.TextField(max_length=255, null=True, blank=True)
     price = models.FloatField(null=False, blank=False)
     stock = models.IntegerField(default=1, null=False, blank=False)
+    category = models.ForeignKey('Category', null=True, blank=False, on_delete=models.SET_NULL, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 
     def __str__(self):
@@ -20,9 +21,18 @@ class Product(models.Model):
 
 
 class Image(models.Model):
-    name = models.ImageField(null=True, blank=True, upload_to='products/images/')
+    name = models.ImageField(null=True, blank=True, upload_to='images/products/')
     product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images')
 
     def __str__(self):
         return f"{self.id}: {self.name}"
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=45, null=False, blank=False, unique=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/categories/')
+    parent = models.ForeignKey('Category', null=True, blank=True, 
+        related_name='subcategories', on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return f" {self.id}: {self.name}"
