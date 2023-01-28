5b31a72116fa6d1d50cda191c14b849fb9d32565
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-01-28 13:25:14 +0000

    Add of all model into admin

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import *
+from django.apps import apps
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Image)
 admin.site.register(Category)
+
+# all other models
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
