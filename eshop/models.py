c2bc8f2cb94927ff0c80578d5fbf188906a187a9
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-01-28 12:39:35 +0000

    Update of model product

--- eshop/models.py
+++ eshop/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    slug = models.CharField(max_length=35, null=True, blank=True, 
+        unique=True)
     description = models.TextField(max_length=255, null=True, blank=True)
     price = models.FloatField(null=False, blank=False)
     stock = models.IntegerField(default=1, null=False, blank=False)
     category = models.ForeignKey('Category', null=True, blank=False, on_delete=models.SET_NULL, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    active = models.BooleanField(default=True, null=False, blank=False)
 
+    class Meta:
+        ordering = ['-created_at']
+    
     def __str__(self):
         return f"{self.id}: {self.name}"
 
