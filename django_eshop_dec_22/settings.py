982d103b4797c91ffa1a325d46a147c588f30f10
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-02-13 18:27:42 +0000

    Make of logout feature

--- django_eshop_dec_22/settings.py
+++ django_eshop_dec_22/settings.py
@@ -138,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGOUT_REDIRECT_URL = reverse_lazy('home')
--- myauth/urls.py
+++ myauth/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
 from myauth import views
 
 urlpatterns = [
     path('login', views.login, name='login'),
+    # path('login', 
+    #     view=LoginView.as_view(template_name='myauth/login.html', 
+    #         redirect_authenticated_user=True), 
+    #     name='login'),
+    path('logout', view=LogoutView.as_view(), name='logout')
 ]
