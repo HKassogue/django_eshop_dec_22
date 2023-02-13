7125a21f282b5e44812058001a067ca9212db067
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-02-13 17:57:50 +0000

    Update for login feature

--- myauth/views.py
+++ myauth/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from .forms import LoginForm
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     form = LoginForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
-        print('is here')
         username = form.cleaned_data["username"]
         password = form.cleaned_data["password"]
         from django.contrib.auth import authenticate, login
         user = authenticate(username=username, password=password)
-        if user is not None:
+        if user:
             login(request, user)
-            return redirect('home', context)
+            return redirect('home')
         else:
-           context['erreur'] = True
-           context['userlog'] = user
+           context['errors'] = True
     return render(request, 'myauth/login.html', context)
