5a15ff288cf0843b30b0e94cbd5a3490b828390d
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-03-06 16:53:36 +0000

    display for chart line

--- eshop/context_processors.py
+++ eshop/context_processors.py
@@ -1,6 +1,12 @@
-from .models import Category
+from .models import Category, Order
 
 def getCategories(request):
     categories = Category.objects.filter(active=True).order_by('name')
     total = sum([category.products.count() for category in categories])
     return {'categories': categories, 'total': total}
+
+def getDashboardData(request):
+    # orders = Order.objects.filter(completed=True, created_at__week__lte=1)
+    week_solde = 100000
+    week_soldes = [500, 2500, 4000, 2000, 1000, 8000, 500]
+    return {'week_solde': week_solde, 'week_soldes': week_soldes}
