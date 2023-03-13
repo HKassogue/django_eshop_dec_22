bfbef8d0821f07f613456dbce77e3199b44acb3e
Christian DIARRA <chrisnrc@MacBook-Pro-de-Christian.local>
2023-03-13 12:28:08 +0000

    message

--- eshop/context_processors.py
+++ eshop/context_processors.py
@@ -1,5 +1,5 @@
-from .models import Category,Order_details,Order
-from datetime import date
+from .models import Category,Order_details,Order, Like, Review 
+from datetime import date, datetime
 
 
 def getCategories(request):
@@ -12,14 +12,27 @@ def getDashboardData(request):
     total_ventes_week = [1000, 2500, 1800, 4000, 5000, 2000, 1000]
     return {'total_ventes': total_ventes, 'total_ventes_week': total_ventes_week}
 
-def getCountProduct(request):
-   current_date=date.today()
-   order=Order.objects.filter(created_at=current_date)
-   somme =0
+def getCountOrderByDay(request):
+   current_date=datetime.now()
+   order=Order.objects.filter(created_at__date = date.today())
+   nombre = order.count()
 #    order_details=order.order_details()
 #    somme = 0
 #    for order in order_details:
 #        somme += order.quantity * order.product.price
- 
-   return  {"somme":somme}
+   return  {"nombre":nombre}
+
+def getCountOrderByMonth(request):
+   current_date=datetime.now()
+   order=Order.objects.filter(created_at__month = date.today().month)
+   month = order.count()
+   return  {"month":month}
+
+def getCountLike(request):
+   like = Like.objects.count()
+   return  {"like":like}
+
+def getCountReview(request):
+   review = Review.objects.count()
+   return  {"review":review}
 
